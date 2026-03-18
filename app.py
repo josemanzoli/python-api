378c20d06a940a50113030dbd9a9954aa9e44ec1
josemanzoli <josemanzoli@gmail.com>
2026-03-17 21:43:59 -0300

    including prometheus metrics

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import uuid
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
 from src.logger import setup_logger
 from src.rabbitmq import rabbitmq_service
+
+# Define as métricas
+REQUEST_COUNT = Counter('http_requests_total', 'Total de requisicoes HTTP', ['method', 'endpoint'])
+MESSAGE_PUBLISHED = Counter('messages_published_total', 'Total de mensagens publicadas com sucesso')
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -12,11 +17,13 @@ def create_app() -> Flask:
 
     @app.get("/health")
     def health_check():
+        REQUEST_COUNT.labels(method='GET', endpoint='/health').inc()
         logger.info("Healthcheck executed", extra={"correlationId": str(uuid.uuid4())})
         return jsonify({"message": "I'm alive"}), 200
 
     @app.post("/message")
     def send_message():
+        REQUEST_COUNT.labels(method='POST', endpoint='/message').inc()
         request_data = request.get_json()
         correlation_id = str(uuid.uuid4())
         
@@ -33,6 +40,7 @@ def create_app() -> Flask:
 
         try:
             rabbitmq_service.publish(new_message)
+            MESSAGE_PUBLISHED.inc()
             logger.info(
                 "Message published to logs exchange",
                 extra={"correlationId": correlation_id, "messageReceived": new_message}
@@ -41,6 +49,10 @@ def create_app() -> Flask:
         except Exception as e:
             logger.error(f"Failed to publish message: {str(e)}")
             return jsonify({"error": "Failed to process message"}), 500
+
+    @app.get("/metrics")
+    def metrics():
+        return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
     return app
 
